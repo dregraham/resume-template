8ec32be64768e9d7bc66701bfd176cc2385f087c
dregraham <99439195+dregraham@users.noreply.github.com>
2025-06-23 16:06:18 -0500

    APIchanges

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -12,7 +12,9 @@ class Portfolio extends Component {
       return (
         <div key={id++} className="columns portfolio-item">
           <div className="item-wrap">
-            <Zmage alt={projects.title} src={projectImage} />
+          <a href={projects.url} target="_blank" rel="noopener noreferrer">
+  <Zmage alt={projects.title} src={projectImage} />
+</a>
             <div style={{ textAlign: "center" }}>{projects.title}</div>
           </div>
         </div>
