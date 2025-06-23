ec210505a8918c8b31c0667bc76a7be75b1f360b
dregraham <99439195+dregraham@users.noreply.github.com>
2025-06-22 19:47:19 -0500

    font changes

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -89,60 +89,60 @@ class Header extends Component {
     };    
     return (
       <header id="home">
-        <ParticlesBg type="custom" config={config} bg={true} />
-        <nav id="nav-wrap">
-          <a className="mobile-btn" href="#nav-wrap" title="Show navigation">
-            Show navigation
+      <ParticlesBg type="custom" config={config} bg={true} />
+      <nav id="nav-wrap">
+        <a className="mobile-btn" href="#nav-wrap" title="Show navigation">
+        Show navigation
+        </a>
+        <a className="mobile-btn" href="#home" title="Hide navigation">
+        Hide navigation
+        </a>
+        <ul id="nav" className="nav">
+        <li className="current">
+          <a className="smoothscroll" href="#home">
+          Home
           </a>
-          <a className="mobile-btn" href="#home" title="Hide navigation">
-            Hide navigation
-          </a>
-          <ul id="nav" className="nav">
-            <li className="current">
-              <a className="smoothscroll" href="#home">
-                Home
-              </a>
-            </li>
-            <li>
-              <a className="smoothscroll" href="#about">
-                About
-              </a>
-            </li>
-            <li>
-              <a className="smoothscroll" href="#resume">
-                Resume
-              </a>
-            </li>
-            <li>
-              <a className="smoothscroll" href="#portfolio">
-                Projects
-              </a>
-            </li>
-            <li>
-              <a className="smoothscroll" href="#contact">
-                Contact
-              </a>
-            </li>
-          </ul>
-        </nav>
-        <div className="row banner">
-          <div className="banner-text">
-            <h1 className="responsive-headline">
-              {name}</h1>
-            <h3>{description}.</h3>
-            <hr />
-            <ul className="social">
-              <a href={github} className="button btn github-btn">
-                <i className="fa fa-github"></i>Github
-              </a>
-            </ul>
-          </div>
-        </div>
-        <p className="scrolldown">
+        </li>
+        <li>
           <a className="smoothscroll" href="#about">
-            <i className="icon-down-circle"></i>
+          About
           </a>
-        </p>
+        </li>
+        <li>
+          <a className="smoothscroll" href="#resume">
+          Resume
+          </a>
+        </li>
+        <li>
+          <a className="smoothscroll" href="#portfolio">
+          Projects
+          </a>
+        </li>
+        <li>
+          <a className="smoothscroll" href="#contact">
+          Contact
+          </a>
+        </li>
+        </ul>
+      </nav>
+      <div className="row banner">
+        <div className="banner-text">
+        <h1 className="responsive-headline">
+          {name}</h1>
+        <h3>{description}.</h3>
+        <hr />
+        <ul className="social">
+          <a href={github} className="button btn github-btn">
+          <i className="fa fa-github"></i>Github
+          </a>
+        </ul>
+        </div>
+      </div>
+      <p className="scrolldown">
+        <a className="smoothscroll" href="#about">
+        <i className="icon-down-circle"></i>
+        </a>
+      </p>
       </header>
     );
   }
